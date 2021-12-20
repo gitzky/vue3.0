da201f2d11c65235d4f2be6ac92fb74bdf044441
gitzky <92792626@qq.com>
2021-12-20 11:40:08 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
-<div id="app">
-  <div style="text-align:center">初始化一个vue 3.0 项目</div>
-  <router-view></router-view>
-</div>
+  <div id="app">
+    <div style="text-align: center">初始化一个vue 3.0 项目</div>
+    <router-view></router-view>
+  </div>
 </template>
 <script>
 export default {
-  name: 'App',
-}
+  name: "App",
+};
 </script>
